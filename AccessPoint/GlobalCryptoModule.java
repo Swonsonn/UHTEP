a69cb3782f63c173179047bca482b26920763a27
Swonsonn <yoda.magistrofjedy@gmail.com>
2021-01-08 13:38:08 +0200

    new parseInt method that doesn't cause error

--- AccessPoint/GlobalCryptoModule.java
+++ AccessPoint/GlobalCryptoModule.java
@@ -19,7 +19,19 @@ public class GlobalCryptoModule {
     private String OutputMessage;
     private String KeyString1;
     private String KeyString2;
-    
+
+    private int parse(String Key){
+        boolean CorrectString=true;
+        for(int i=0;i<Key.length();++i)
+            if(Key.charAt(i)<48 || Key.charAt(i)>57){CorrectString=false;i+=Key.length();}
+        if(CorrectString)return Integer.parseInt(Key);
+        int temp=0;
+        for(int i=0;i<Key.length();++i) {
+            temp+=Key.charAt(i);
+        }
+        return temp;
+    }
+
     public String crypt(String InputMessage, int method, String KeyString1, String KeyString2){
         this.InputMessage=InputMessage;
         this.KeyString1=KeyString1;
@@ -27,7 +39,7 @@ public class GlobalCryptoModule {
         
         switch(method){
             case 1:{
-                caesar.Set(InputMessage, Integer.parseInt(KeyString1));
+                caesar.Set(InputMessage, parse(KeyString1));
                 caesar.MessageCrypt();
                 OutputMessage=caesar.GetMessage();
                 break;
@@ -39,18 +51,18 @@ public class GlobalCryptoModule {
                 break;
             }
             case 3:{
-                socrates.Set(InputMessage, Integer.parseInt(KeyString1));
+                socrates.Set(InputMessage, parse(KeyString1));
                 socrates.MessageCrypt();
                 OutputMessage=socrates.GetMessage();
                 break;
             }
             case 4:{
-                id.Set(InputMessage, Integer.parseInt(KeyString1));
+                id.Set(InputMessage, parse(KeyString1));
                 id.MessageCrypt();
                 OutputMessage=id.GetMessage();break;
             }
             case 5:{
-                itajara.Set(InputMessage, Integer.parseInt(KeyString1));
+                itajara.Set(InputMessage, parse(KeyString1));
                 itajara.MessageCrypt();
                 OutputMessage=itajara.GetMessage();break;
             }
@@ -60,12 +72,12 @@ public class GlobalCryptoModule {
                 OutputMessage=superid.GetMessage();break;
             }
             case 7:{
-                heraklesz.Set(InputMessage, Integer.parseInt(KeyString1), Integer.parseInt(KeyString2));
+                heraklesz.Set(InputMessage, parse(KeyString1), parse(KeyString2));
                 heraklesz.MessageCrypt();
                 OutputMessage=heraklesz.GetMessage();break;
             }
             case 8:{
-                ape.Set(InputMessage, Integer.parseInt(KeyString1));
+                ape.Set(InputMessage, parse(KeyString1));
                 ape.MessageCrypt();
                 OutputMessage=ape.GetMessage();break;
             }
@@ -80,7 +92,7 @@ public class GlobalCryptoModule {
         
         switch(method){
             case 1:{
-                caesar.Set(InputMessage, Integer.parseInt(KeyString1));
+                caesar.Set(InputMessage, parse(KeyString1));
                 caesar.MessageDecrypt();
                 OutputMessage=caesar.GetMessage();break;
             }
@@ -90,17 +102,17 @@ public class GlobalCryptoModule {
                 OutputMessage=diogenes.GetMessage();break;
             }
             case 3:{
-                socrates.Set(InputMessage, Integer.parseInt(KeyString1));
+                socrates.Set(InputMessage, parse(KeyString1));
                 socrates.MessageDecrypt();
                 OutputMessage=socrates.GetMessage();break;
             }
             case 4:{
-                id.Set(InputMessage, Integer.parseInt(KeyString1));
+                id.Set(InputMessage, parse(KeyString1));
                 id.MessageDecrypt();
                 OutputMessage=id.GetMessage();break;
             }
             case 5:{
-                itajara.Set(InputMessage, Integer.parseInt(KeyString1));
+                itajara.Set(InputMessage, parse(KeyString1));
                 itajara.MessageDecrypt();
                 OutputMessage=itajara.GetMessage();break;
             }
@@ -110,12 +122,12 @@ public class GlobalCryptoModule {
                 OutputMessage=superid.GetMessage();break;
             }
             case 7:{
-                heraklesz.Set(InputMessage, Integer.parseInt(KeyString1), Integer.parseInt(KeyString2));
+                heraklesz.Set(InputMessage, parse(KeyString1), parse(KeyString2));
                 heraklesz.MessageDecrypt();
                 OutputMessage=heraklesz.GetMessage();break;
             }
             case 8:{
-                ape.Set(InputMessage, Integer.parseInt(KeyString1));
+                ape.Set(InputMessage, parse(KeyString1));
                 ape.MessageDecrypt();
                 OutputMessage=ape.GetMessage();break;
             }
---
+++ Main.java
@@ -0,0 +1,30 @@
+import AccessPoint.GlobalCryptoModule;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String fileName=args[0];
+        File file=new File(fileName);
+        Scanner inPut=new Scanner(file);
+        GlobalCryptoModule module=new GlobalCryptoModule();
+
+        String MESSAGE="pepega",KEY1="1",KEY2="2",METHOD="1",WAY="crypt";
+        WAY=args[1];
+        METHOD=args[2];
+        MESSAGE=inPut.nextLine();
+        KEY1=inPut.nextLine();
+        KEY2=inPut.nextLine();
+        inPut.close();
+
+        if(WAY.equals("crypt"))MESSAGE=module.crypt(MESSAGE,Integer.parseInt(METHOD),KEY1,KEY2);
+        else MESSAGE=module.decrypt(MESSAGE,Integer.parseInt(METHOD),KEY1,KEY2);
+
+        FileWriter outPut=new FileWriter(fileName);
+        outPut.write(MESSAGE+"\n"+KEY1+"\n"+KEY2);
+        outPut.close();
+    }
+}
